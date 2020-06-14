56cca8d84f612b7c834caf31b316669bdd298131
atifsultani <atifsultani89@gmail.com>
2020-06-14 17:04:16 +0500

    third GIT

---
+++ app.js
@@ -0,0 +1,178 @@
+// chapter 1 ALERTS
+
+// task 1
+// alert("Error! Please enter a valid password");
+
+// task 2
+// alert('Welcome to js LAnd...\nHappy Coding!');
+
+// task 3
+// alert("welcome to js Land....")
+
+// task 4
+// console.log('hello.. i can run Js through my web browser s console');
+
+// task 5
+
+ // chapter 2 VARIABLES FOR STRINGS
+
+ //task 1
+
+ // var name = "Jhone Doe";
+// alert(name);
+
+//task 2
+// var age = "15 years old";
+// alert(age);
+
+//task 3
+// var crt = "Certified Mobile Applicaiton Developemnt";
+// alert(crt);
+
+//task 4
+// alert('PIZZA\nPIZZ\nPIZ\nPI\nP');
+
+//task 5
+// var msg =  "My email address is";
+// var email = "example@example.com";
+// alert(msg + " " + email);
+
+//task 6
+// var a = "I am trying to learn the Book A smarter way to learn javaScrip";
+// alert(a);
+// var i ="Yah! I can write HTML content through javaScript";
+// document.write(i);
+
+// chapter 3 VARIABLES FOR NUMBER
+
+//task 1
+//  var age = "I am 15 years old";
+//  alert(age);
+
+//task 2
+// var visitor = "YOU have visited this site 14 times";
+//  alert(visitor);
+
+//task 3
+// document.write("My birth year is 1990<br>");
+// document.write("DATA type of my ddeclared variable is number")
+
+//task 4
+// document.write("<b>John Doe</b> ordered <b>5 T-shirt</b>(s) on XYZ Clothing store");
+
+// chapter 4 VARIABLE NAMES: LEGAL & ILLEGAL
+
+// legal 
+// Variable names can only contain  number , $ , and _.For example $my_1stVariablec)
+// legal
+// Variables must begin with a letter, $ or_. For example $name, _name or named)
+// Variable names are case sesitive
+// Variable names should not be JS Keyword
+
+
+// chapter 5 MATH EXPRESSIONS
+//TASK 1 add
+
+// var a = 3;
+// b = 5+a;
+// alert(b);
+
+//TASK 2 Subtraction
+
+// var a = 3;
+// b = 5-a;
+// alert(b);
+
+//TASK 3 multiplication
+// var a = 3;
+// b = 5*a;
+// alert(b);
+
+//TASK 4 division
+// var a = 5 % 3;
+// alert(a);
+
+//TASK 5 Cost of one movire ticket
+
+// var a = 600;
+// b = 5*a;
+// alert(b);
+
+// TASK 6 multiplication table 
+// var a=prompt("Enter Your Number")
+// for(var s=1;s<=10;s++)
+// {
+//     document.write(a+" * "+s+" = "+a*s+"<br/>");
+// }
+// TASK MARKS SHEET
+// var totalMask =prompt("Enter Your Total mask");
+// var obtained_mask =prompt("Enter Your Obtained mask");
+// var percentage = obtained_mask * 100 / 100;
+// document.write("your Total masks is:" + obtained_mask + "<br/>");
+// document.write("your percentage is:" + percentage + "<br/>");
+
+//Chapter 6 
+//task 1
+// var a = 10;
+// var b = ++a + a + a++ + ++a + --a + a + a-- - a
+//alert(b);
+//Chapter 9
+//task 1
+// var c = prompt("Enter city name")
+// if (c === "karachi"){
+//     alert("Welcome to city of lights")
+// }
+// else{
+//     alert("Your are not Allow")
+// }
+//  var c = prompt("Enter Your Gender")
+ 
+//  if (c === "male"){
+//      alert("Good MOrning Sir")
+// }
+ 
+
+// else{
+//      alert("Your are not Allow")
+//  }
+
+//  var d = prompt("Enter Your Gender")
+//   if (d === "female"){
+//     alert("Good Morning Ma’am")
+//   }
+//   else{
+//     alert("Your are not Allow")
+// }
+
+//  var c = prompt("input color of road traffic signal")
+ 
+//  if (c === "red"){
+//      alert("Must Stop")
+// }
+ 
+
+// else{
+//      alert("Your are not Allow")
+//  }
+
+// var c = prompt("input color of road traffic signal")
+ 
+// if (c === "yellow"){
+//     alert("Ready to move")
+// }
+
+
+// else{
+//     alert("Your are not Allow")
+// }
+
+// var c = prompt("input color of road traffic signal")
+ 
+// if (c === "green"){
+//     alert("Move now")
+// }
+
+
+// else{
+//     alert("Your are not Allow")
+// }
